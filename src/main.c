3f137bb3850801421b5ab53d2ed353e4b7578ed3
abvega <abvega@miners.utep.edu>
2022-09-19 12:44:58 -0600

    progress so far

---
+++ src/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+  char strng[100];
+  while(1){
+    putchar('$');
+  while(fgets(strng,sizeof(strng),stdin)){
+    fputs(strng,stdout);
+    break;
+  }
+  }
+}
+

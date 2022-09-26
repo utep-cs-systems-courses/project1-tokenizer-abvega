8218bf2de9c039b14e1d8c6d220872006ac86af0
abvega <89613286+abvega@users.noreply.github.com>
2022-09-26 16:26:02 -0600

    Add files via upload

--- src/history.c
+++ src/history.c
@@ -19,17 +19,18 @@ void add_history(List *list, char *str){
         num++;
     }
     temp->next = malloc(sizeof(Item));
-    temp->next->str = copy_str(str,string_length(str));
+    temp->next->str = copy_str(str,lengthString(str));
     temp->next->id =num;
 }
 char *get_history(List *list, int id){
     Item *temp = list->root;
-    while(temp!=0){
-        if(temp->id == id){
-            return temp->next;
-        }
+    if(temp == NULL){
+        return NULL;
     }
-    return "Item not found";
+    while(temp && temp->id !=id){
+        temp = temp->next;
+    }
+    return temp->str;
 }
 void print_history(List *list){
     Item *temp = list->root->next;

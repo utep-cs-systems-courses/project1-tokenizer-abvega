62f1398c2931b9b8b97407cbd108e32e3843f965
abvega <89613286+abvega@users.noreply.github.com>
2022-09-25 21:18:52 -0600

    Add files via upload

--- src/main.c
+++ src/main.c
@@ -1,13 +1,23 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "history.h"
+#include "tokenizer.h"
+
 int main(){
-  char strng[100];
-  while(1){
-    putchar('$');
-  while(fgets(strng,sizeof(strng),stdin)){
-    fputs(strng,stdout);
-    break;
+  char input[100];
+  List *hist = init_history();
+  printf("Please select 1. to tokenize words or 2. to see the history");
+  fgets(input, 100, stdin);
+  int select = atoi(input);
+  if(select == 1){
+    printf("Enter your sentence: \n");
+    fgets(select, 100, stdin);
+    add_history(hist, input);
+    char **tokens = tokenize(input);
+    print_tokens(tokens);
   }
+  else if(select == 2){
+    print_history(hist);
   }
 }
 
